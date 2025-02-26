82a93d276c82b8e548e737e17bb03a67bd34a9ca
rdmultiservicios <dalfre@live.com>
2025-02-26 15:17:43 -0500

    Update script.js

--- cart/script.js
+++ cart/script.js
@@ -126,10 +126,10 @@ buyNowButton.addEventListener("click", () => {
     // Definimos una variable para construir el mensaje
     let mensaje = "Hola, quiero comprar los siguientes productos:\n\n";
 
-    cartBoxes.forEach(cartBox => {
-        mensaje += `${cartBox.productTitle} 
-        - Cantidad: ${cartBox.quantity} 
-        - Precio: S/${(cartBox.productPrice * cartBox.quantity)
+    productBoxes.forEach(productBox => {
+        mensaje += `${productBox.productTitle} 
+        - Cantidad: ${productBox.quantity} 
+        - Precio: S/${(productBox.productPrice * productBox.quantity)
             .toFixed(2)}\n`;
     });
     
